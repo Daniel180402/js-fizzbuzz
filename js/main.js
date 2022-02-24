6f72927c9d6e314bfdce33043f00073b2acedd0c
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-02-24 13:36:56 +0100

    added button with event listener

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-for (let i = 1; i < 101; i++){
-    if ((i % 3 == 0) && (i % 5 == 0)){
-        console.log("FizzBuzz");
+const generatorbtn = document.getElementById("generator-button");
+generatorbtn.addEventListener("click", function(){
+
+    for (let i = 1; i < 101; i++){
+        if ((i % 3 == 0) && (i % 5 == 0)){
+            console.log("FizzBuzz");
+        }
+        else if (i % 5 == 0){
+            console.log("Buzz");
+        }
+        else if (i % 3 == 0){
+            console.log("Fizz");
+        }
+        else{
+            console.log(i);
+        }
     }
-    else if (i % 5 == 0){
-        console.log("Buzz");
-    }
-    else if (i % 3 == 0){
-        console.log("Fizz");
-    }
-    else{
-        console.log(i);
-    }
-}
+})
